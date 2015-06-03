a582917c058484f5ccfc084835c525c55db99a8e
Aterocana <dominicimaurizio@gmail.com>
2015-06-03 12:49:51 +0200

    temperature script

---
+++ temperature.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Copyleft (C) 2015 Maurizio Dominici <dominicimaurizio@gmail.com> 
+# www.digitalsyncretism.com
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+
+now=$(date)
+
+if [ -f "/proc/uptime" ]; then
+	uptime=$(cat /proc/uptime)
+	uptime=${uptime%%.*}
+	seconds=$(( uptime%60 ))
+	minutes=$(( uptime/60%60 ))
+	hours=$(( uptime/60/60%24 ))
+	days=$(( uptime/60/60/24 ))
+	uptime="$days days, $hours hours, $minutes minutes, $seconds seconds"
+else
+	uptime=""
+fi
+
+temp=$(/opt/vc/bin/vcgencmd measure_temp)
+
+echo $now
+echo $uptime
+echo $temp
+
+echo -e "{\"date\":\""$(date)"\",\t\"temp\":\""$temp"\",\t\"uptime\":\""$uptime"\"}"
